b84310383d3e54d74b1e26dab72541f26404b368
StartOfDeveloper <127432151+StartOfDeveloper@users.noreply.github.com>
2023-03-09 19:18:55 +0200

    My first site

---
+++ JS/Script.js
@@ -0,0 +1,30 @@
+let slideSwiper = new Swiper('.swiper', {
+    simulateTouch: true,
+    grabCursor: true,
+    slidesPerView: 4.5,
+    centeredslides: true,
+    initialslide: 2,
+    freeMode: true,
+    breakpoints: {
+        992: {
+            slidesPerview: 3.3,
+            },
+        480: {
+             slidesPerview: 2.9,
+            },
+        320: {
+        slidesPerView: 2.3,
+            },
+        },
+});
+
+const burgerMenu = document.querySelector('.header-nuv-menu');
+const burgerMenuItems = document.querySelector('.nav');
+const navBlock = document.querySelector('.nav-block')
+bodyElement = document.documentElement.lastChild;
+
+if (burgerMenu) {
+    burgerMenu.addEventListener('click', function (e) {
+            bodyElement.classList.toggle('bodyMenu');
+    })
+}
